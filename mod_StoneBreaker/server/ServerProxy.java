5047e865932cbfd1f97e0ae8c57d6be85db760b5
dewfalse <dewfalse@gmail.com>
2012-10-28 13:54:33 +0900

    コマンドを登録

--- mod_StoneBreaker/server/ServerProxy.java
+++ mod_StoneBreaker/server/ServerProxy.java
@@ -1,7 +1,26 @@
 package mod_StoneBreaker.server;
 
-import mod_TreeBreaker.CommonProxy;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.src.ServerCommandManager;
+import cpw.mods.fml.server.FMLServerHandler;
+import mod_StoneBreaker.CommonProxy;
 
 public class ServerProxy extends CommonProxy {
+	@Override
+	public void registerCommand() {
+
+		if(FMLServerHandler.instance() == null) {
+			return;
+		}
+
+		if(FMLServerHandler.instance().getServer() == null) {
+			return;
+		}
+		if(FMLServerHandler.instance().getServer().getCommandManager() instanceof ServerCommandManager) {
+			ServerCommandManager manager = (ServerCommandManager)FMLServerHandler.instance().getServer().getCommandManager();
+			manager.registerCommand(new CommandTarget());
+            MinecraftServer.logger.info("Register StoneBreaker Command(/StoneBreaker target add/remove (blockID)");
+		}
+	}
 
 }
