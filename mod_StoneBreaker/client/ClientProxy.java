0f8363775921587607f866fe066c04e3d31d82db
dewfalse <dewfalse@gmail.com>
2012-10-28 23:10:21 +0900

    クライアントProxyにキーバインドとTickハンドラーを追加

--- mod_StoneBreaker/client/ClientProxy.java
+++ mod_StoneBreaker/client/ClientProxy.java
@@ -1,7 +1,22 @@
 package mod_StoneBreaker.client;
 
-import mod_TreeBreaker.CommonProxy;
+import cpw.mods.fml.client.registry.KeyBindingRegistry;
+import cpw.mods.fml.common.Side;
+import cpw.mods.fml.common.registry.TickRegistry;
+import mod_StoneBreaker.CommonProxy;
+import mod_StoneBreaker.client.ClientKeyHandler;
+import mod_StoneBreaker.client.ClientTickHandler;
 
 public class ClientProxy extends CommonProxy {
+
+	@Override
+	public void addKeyBinding() {
+		KeyBindingRegistry.registerKeyBinding(new ClientKeyHandler());
+	}
+
+	@Override
+	public void registerTickHandler() {
+		TickRegistry.registerTickHandler(new ClientTickHandler(), Side.CLIENT);
+	}
 
 }
